0db8d4529591bd81640a30d7882e0e5708e673a6
styin8 <531535977@qq.com>
2023-10-22 21:13:39 +0800

    add grcnn model

---
+++ models/grcnn_model.py
@@ -0,0 +1,69 @@
+from .base_model import BaseModel
+import torch
+import torch.nn.functional as F
+from utils import evaluation
+
+
+class GRCNNModel(BaseModel):
+    def __init__(self, opt):
+        BaseModel.__init__(self, opt)
+
+    def load_network(self):
+        from .networks import GenerativeResnet
+        input_channels = self.opt.enable_rgb * 3 + self.opt.enable_depth
+        self.net = GenerativeResnet(input_channels).to(self.device)
+        if self.isTrain:
+            self.optimizer = torch.optim.Adam(
+                self.net.parameters())
+        return self.net
+
+    def set_input(self, x, gt):
+        self.x = x.to(self.device)
+        self.gt = [y.to(self.device) for y in gt]
+
+    def forward(self):
+        self.pred = self.net(self.x)
+        self.compute_loss()
+
+    def compute_loss(self):
+        gt_pos, gt_sin, gt_cos, gt_width = self.gt
+        pred_pos, pred_sin, pred_cos, pred_width = self.pred
+
+        loss_pos = F.smooth_l1_loss(pred_pos, gt_pos)
+        loss_sin = F.smooth_l1_loss(pred_sin, gt_sin)
+        loss_cos = F.smooth_l1_loss(pred_cos, gt_cos)
+        loss_width = F.smooth_l1_loss(pred_width, gt_width)
+
+        self.loss = loss_pos + loss_sin + loss_cos + loss_width
+        self.losses = {
+            'loss_pos': loss_pos,
+            'loss_sin': loss_sin,
+            'loss_cos': loss_cos,
+            'loss_width': loss_width
+        }
+
+        self.pred = {'pos': pred_pos,
+                     'sin': pred_sin,
+                     'cos': pred_cos,
+                     'width': pred_width}
+
+    def backward(self):
+        self.loss.backward()
+
+    def optimize_parameters(self):
+        self.forward()
+        self.optimizer.zero_grad()
+        self.backward()
+        self.optimizer.step()
+
+    def test(self, val_data, didx, rot, zoom_factor):
+        self.forward()
+        q_out, ang_out, w_out = self.post_process_output(self.pred['pos'], self.pred['sin'],
+                                                         self.pred['cos'], self.pred['width'])
+        s = evaluation.calculate_iou_match(q_out, ang_out,
+                                           val_data.dataset.get_gtbb(
+                                               didx, rot, zoom_factor),
+                                           no_grasps=1,
+                                           grasp_width=w_out,
+                                           )
+        return s
--- models/networks.py
+++ models/networks.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+# GGCNN network
 class GGCNN(nn.Module):
     """
     GG-CNN
@@ -52,6 +53,7 @@ class GGCNN(nn.Module):
         return pos_output,  sin_output, cos_output, width_output
 
 
+# GGCNN2 network
 class GGCNN2(nn.Module):
     def __init__(self, input_channels=1, l3_k_size=5):
         filter_sizes = [16,  # First set of convs
@@ -116,3 +118,108 @@ class GGCNN2(nn.Module):
 
         return pos_output, sin_output, cos_output, width_output
 
+
+# GRCNN and GRCNN2 network
+class GraspModel(nn.Module):
+    """
+    An abstract model for grasp network in a common format.
+    """
+
+    def __init__(self):
+        super(GraspModel, self).__init__()
+
+    def forward(self, x_in):
+        raise NotImplementedError()
+
+    def predict(self, xc):
+        pos_pred, cos_pred, sin_pred, width_pred = self(xc)
+        return {
+            'pos': pos_pred,
+            'cos': cos_pred,
+            'sin': sin_pred,
+            'width': width_pred
+        }
+
+
+class ResidualBlock(nn.Module):
+    """
+    A residual block with dropout option
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size=3):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels,
+                               kernel_size, padding=1)
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        self.conv2 = nn.Conv2d(in_channels, out_channels,
+                               kernel_size, padding=1)
+        self.bn2 = nn.BatchNorm2d(in_channels)
+
+    def forward(self, x_in):
+        x = self.bn1(self.conv1(x_in))
+        x = F.relu(x)
+        x = self.bn2(self.conv2(x))
+        return x + x_in
+
+
+class GenerativeResnet(GraspModel):
+
+    def __init__(self, input_channels=1, prob=0.0):
+        super(GenerativeResnet, self).__init__()
+        self.conv1 = nn.Conv2d(
+            input_channels, 32, kernel_size=9, stride=1, padding=4)
+        self.bn1 = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        self.res1 = ResidualBlock(128, 128)
+        self.res2 = ResidualBlock(128, 128)
+        self.res3 = ResidualBlock(128, 128)
+        self.res4 = ResidualBlock(128, 128)
+        self.res5 = ResidualBlock(128, 128)
+
+        self.conv4 = nn.ConvTranspose2d(
+            128, 64, kernel_size=4, stride=2, padding=1, output_padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+
+        self.conv5 = nn.ConvTranspose2d(
+            64, 32, kernel_size=4, stride=2, padding=2, output_padding=1)
+        self.bn5 = nn.BatchNorm2d(32)
+
+        self.conv6 = nn.ConvTranspose2d(
+            32, 32, kernel_size=9, stride=1, padding=4)
+
+        self.pos_output = nn.Conv2d(32, 1, kernel_size=2)
+        self.cos_output = nn.Conv2d(32, 1, kernel_size=2)
+        self.sin_output = nn.Conv2d(32, 1, kernel_size=2)
+        self.width_output = nn.Conv2d(32, 1, kernel_size=2)
+
+        self.dropout1 = nn.Dropout(p=prob)
+
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d)):
+                nn.init.xavier_uniform_(m.weight, gain=1)
+
+    def forward(self, x_in):
+        x = F.relu(self.bn1(self.conv1(x_in)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.res1(x)
+        x = self.res2(x)
+        x = self.res3(x)
+        x = self.res4(x)
+        x = self.res5(x)
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = self.conv6(x)
+
+        pos_output = self.pos_output(self.dropout1(x))
+        cos_output = self.cos_output(self.dropout1(x))
+        sin_output = self.sin_output(self.dropout1(x))
+        width_output = self.width_output(self.dropout1(x))
+
+        return pos_output, sin_output, cos_output, width_output
